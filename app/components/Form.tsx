153b37c1e425e81cb8c7a93ddbc1cb523f0ae173
giorgi kapanadze <georgegiorgika@gmail.com>
2025-03-03 21:59:48 +0000

    add form and inputs heigt width and lenght

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,3 +1,30 @@
 export default function FillForm() {
-  return <div className="">Form Here</div>;
+  return (
+    <div className="">
+      <form className=" flex flex-col gap-3 ">
+        <input
+          type="text"
+          placeholder="Enter item number..."
+          className="w-full px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
+        />
+        <div className=" flex gap-1.5 w-full justify-between">
+          <input
+            type="text"
+            placeholder="height"
+            className="w-28 px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
+          />
+          <input
+            type="text"
+            placeholder="length"
+            className="w-28 px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
+          />
+          <input
+            type="text"
+            placeholder="width"
+            className="w-28 px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
--- app/pages/form/page.tsx
+++ app/pages/form/page.tsx
@@ -2,7 +2,10 @@ import FillForm from "@/app/components/Form";
 
 export default function page() {
   return (
-    <div className=" flex justify-center bg-[#121212] !mt-5">
+    <div className=" w-full  flex flex-col items-center justify-center bg-[#121212]  !mt-20   ">
+      <h1 className=" !mb-10 filter blur-[0.28px] text-xl font-semibold">
+        Prepare Your Task
+      </h1>
       <FillForm />
     </div>
   );
