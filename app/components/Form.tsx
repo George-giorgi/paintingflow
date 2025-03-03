fa1af7444d0af58df6c1a9842f646184ac04bbc4
giorgi kapanadze <georgegiorgika@gmail.com>
2025-03-03 22:14:18 +0000

    add weight input and improve responsive

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -5,24 +5,34 @@ export default function FillForm() {
         <input
           type="text"
           placeholder="Enter item number..."
-          className="w-full px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
+          className=" w-full px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
         />
-        <div className=" flex gap-1.5 w-full justify-between">
-          <input
-            type="text"
-            placeholder="height"
-            className="w-28 px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
-          />
-          <input
-            type="text"
-            placeholder="length"
-            className="w-28 px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
-          />
-          <input
-            type="text"
-            placeholder="width"
-            className="w-28 px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
-          />
+        <div className=" flex md:flex-row flex-col gap-2 w-full justify-between">
+          <div className=" flex gap-2">
+            <input
+              type="text"
+              placeholder="height"
+              className="w-28 px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
+            />
+            <input
+              type="text"
+              placeholder="length"
+              className="w-28 px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
+            />
+          </div>
+
+          <div className=" flex gap-2">
+            <input
+              type="text"
+              placeholder="width"
+              className="w-28 px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
+            />
+            <input
+              type="text"
+              placeholder="weight"
+              className="w-28 px-2 py-2 text-center border-b border-gray-300 focus:outline-none transition-colors duration-150 ease-in-out appearance-none focus:placeholder-opacity-0"
+            />
+          </div>
         </div>
       </form>
     </div>
