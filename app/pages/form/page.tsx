dc9ceca04ec9ec4d6928fa68d299b8742eafa5e5
Breandan McCann <Breandan.McCann@mallaghangroup.com>
2025-03-04 12:14:21 +0000

    add mui material icons and ui task icons from mui

--- app/pages/form/page.tsx
+++ app/pages/form/page.tsx
@@ -1,10 +1,13 @@
+"use client";
+
 import FillForm from "@/app/components/Form";
+import { PendingActions } from "@mui/icons-material";
 
 export default function page() {
   return (
     <div className=" w-full  flex flex-col items-center justify-center bg-[#121212]  !mt-20   ">
       <h1 className=" !mb-10 filter blur-[0.28px] text-xl font-semibold">
-        Prepare Your Task
+        Prepare Your Task <PendingActions />
       </h1>
       <FillForm />
     </div>
